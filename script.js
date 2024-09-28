80ecb1b5a0747503bf66af5708c19ecc6b5ebcf9
Angelo <113689254+Gelatino0308@users.noreply.github.com>
2024-09-28 20:02:42 +0800

    feat: add chess board color pattern on UI board

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const board = document.querySelector("#chess-board");
+const cellArr = [];
 let boardSize = 8;
 
 for (let i = 0; i < boardSize; i++){
@@ -9,6 +10,15 @@ for (let i = 0; i < boardSize; i++){
     for (let j = 0; j < boardSize; j++) {
         const cell = document.createElement("div");
         cell.classList.add("grid-cell");
+
+        if (i % 2 === 0 && j % 2 === 1 || i % 2 === 1 && j % 2 === 0) {
+            cell.classList.add("darkcells");
+        }
+        else {
+            cell.classList.add("lightcells");
+        }
+
+        cellArr.push(cell);
         row.appendChild(cell);
     }
 }
